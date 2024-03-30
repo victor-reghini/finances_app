1c0d2fe1bd357ef50fd673129983a88c32214e9b
Victor Silva <victor.hugo.mrts@gmail.com>
2024-03-29 23:21:05 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # finances_app
 personal finances app
+
+### TODO list:
+
+- [x] Banco de dados
+- [ ] API base (CRUD básico)
+- [ ] Front base
+- [ ] Login e autenticação
+- [ ] Validações e Testes
+- [ ] Front gráficos
+- [ ] Implementar planejamento financeiro
+
+
+## Database
+
+- [x] Criação das tabelas de usuários, contas, categorias, ...
+- [x] Criação das sequences de usuários, contas, categorias, ...
+- [x] Criação das chaves estrangeiras
+
+
+## API
+
+- [ ] Criação de usuários, contas, categorias, ...
+- [ ] Leitura de usuários, contas, categorias, ...
+- [ ] Edição de usuários, contas, categorias, ...
+- [ ] Remoção de usuários, contas, categorias, ...
+- [ ] Validação dos dados recebidos
+- [ ] Login, Autenticação e Segurança
+
+## Front-end
+
+- [ ] Modal de criação de usuários, contas, gastos, ...
+- [ ] Tela de monitoramento de gastos
+- [ ] Visão de tabela
+- [ ] Visão de gráficos
+- [ ] Filtros e pesquisa avançada
---
+++ finances_app_inserts.sql
@@ -0,0 +1,57 @@
+
+INSERT INTO users
+(id, "name", email, "password")
+VALUES(0, '', '', '');
+
+
+INSERT INTO expense_group
+(id, "name", description, user_id)
+VALUES(0, '', '', 0);
+
+
+INSERT INTO accounts
+(id, "name", description, user_id, balance, status)
+VALUES(0, '', '', 0, 0, '');
+
+
+INSERT INTO categories
+(id, "name", description, category_type, user_id)
+VALUES(0, '', '', '', 0);
+
+
+INSERT INTO cards
+(id, "name", description, account_id, user_id, closing_date, card_type_id)
+VALUES(0, '', '', 0, 0, 0, 0);
+
+
+INSERT INTO card_type
+(id, "name", description, account_id, user_id)
+VALUES(0, '', '', 0, 0);
+
+
+INSERT INTO payment_type
+(id, "name", description, user_id)
+VALUES(0, '', '', 0);
+
+
+INSERT INTO expenses
+(id, title, description, status, value, paid_value, payment_date, payment_due_date, purchase_date, installment, total_instalments, payment_type_id, account_id, card_id, user_id, expense_group_id, category_id)
+VALUES(0, '', '', '', 0, 0, '', '', '', 0, 0, 0, 0, 0, 0, 0, 0);
+
+
+INSERT INTO incomes
+(id, title, description, value, payment_date, payment_type_id, account_id, user_id, category_id)
+VALUES(0, '', '', 0, '', 0, 0, 0, 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
