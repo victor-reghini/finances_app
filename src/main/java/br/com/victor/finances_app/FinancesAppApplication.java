ea56797e989973da42c0cc157796e0915623b0c6
Victor Silva <victor.hugo.mrts@gmail.com>
2024-03-31 22:51:49 -0300

    subindo base do projeto spring

---
+++ src/main/java/br/com/victor/finances_app/FinancesAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor.finances_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesAppApplication.class, args);
+	}
+
+}
